5920ec419e574f1cbf708e01a1506786d133d20f
Ivan Andreyko <myromeu@ya.ru>
2025-05-19 23:12:00 +0400

    hw-7

---
+++ interfaces/homework_test.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// go test -v homework_test.go
+
+type UserService struct {
+	// not need to implement
+	NotEmptyStruct bool
+}
+type MessageService struct {
+	// not need to implement
+	NotEmptyStruct bool
+}
+
+type Container struct {
+	registry map[string]func() interface{}
+}
+
+func NewContainer() *Container {
+	return &Container{
+		registry: make(map[string]func() interface{}),
+	}
+}
+
+func (c *Container) RegisterType(name string, constructor func() interface{}) {
+	c.registry[name] = constructor
+}
+
+func (c *Container) Resolve(name string) (interface{}, error) {
+	constructor, ok := c.registry[name]
+	if !ok {
+		return nil, fmt.Errorf("no constructor for %s", name)
+	}
+	return constructor(), nil
+}
+
+func TestDIContainer(t *testing.T) {
+	container := NewContainer()
+	container.RegisterType("UserService", func() interface{} {
+		return &UserService{}
+	})
+	container.RegisterType("MessageService", func() interface{} {
+		return &MessageService{}
+	})
+
+	userService1, err := container.Resolve("UserService")
+	assert.NoError(t, err)
+	userService2, err := container.Resolve("UserService")
+	assert.NoError(t, err)
+
+	u1 := userService1.(*UserService)
+	u2 := userService2.(*UserService)
+	assert.False(t, u1 == u2)
+
+	messageService, err := container.Resolve("MessageService")
+	assert.NoError(t, err)
+	assert.NotNil(t, messageService)
+
+	paymentService, err := container.Resolve("PaymentService")
+	assert.Error(t, err)
+	assert.Nil(t, paymentService)
+}
